4a15cd294b7d1bc828891be722091349baf65184
iamwinner422 <iamwinner422@gmail.com>
2024-08-20 14:38:08 +0000

    Allo allo

--- nest/src/auth/auth.service.ts
+++ nest/src/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { SignupDto } from './dto/signupDto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import e from 'express';
 
 @Injectable()
 export class AuthService {
-    async signup(dto: SignupDto){
-
+    constructor(private readonly prismaService: PrismaService) {}
+    async signup(signupDto: SignupDto){
+        const {email, username, password} = signupDto;
+        const user = await this.prismaService.user.findUnique({
+            where: {email}
+        })
     }
 }
--- nest/src/prisma/prisma.service.ts
+++ nest/src/prisma/prisma.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
+
+
 @Injectable()
 export class PrismaService extends PrismaClient {
     constructor(configService: ConfigService){
