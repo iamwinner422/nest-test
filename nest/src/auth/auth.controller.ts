f266aa1f8bd2a2095cbbb7adbfe46d191d8c22d9
iamwinner422 <iamwinner422@gmail.com>
2024-08-20 16:02:50 +0000

    Debut du sign in

--- nest/src/auth/auth.controller.ts
+++ nest/src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignupDto } from './dto/signupDto';
 import { AuthService } from './auth.service';
+import { SigninDto } from './dto/signinDto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
     @Post('signup')
     signup(@Body() dto: SignupDto) {
-        return 'ok';
+        return this.authService.signup(dto)
+    }
+
+    signin(@Body() dto: SigninDto){
+        return this.authService.signin(dto)
     }
 }
--- nest/src/auth/auth.service.ts
+++ nest/src/auth/auth.service.ts
@@ -2,9 +2,14 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { SignupDto } from './dto/signupDto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt'
+import { MailerService } from 'src/mailer/mailer.service';
+import { SigninDto } from './dto/signinDto';
 @Injectable()
 export class AuthService {
-    constructor(private readonly prismaService: PrismaService) {}
+    constructor(
+        private readonly prismaService: PrismaService,
+        private readonly mailerService: MailerService
+    ) {}
     async signup(signupDto: SignupDto){
         const {email, username, password} = signupDto;
         const user = await this.prismaService.user.findFirst({
@@ -15,6 +20,11 @@ export class AuthService {
         await this.prismaService.user.create({
             data: {email, username, password: hash}
         });
+        await this.mailerService.sendSignupConfirmation(email);
         return {data: 'User created'}
+    }
+
+    async signin(signInDto: SigninDto){
+        
     }
 }
